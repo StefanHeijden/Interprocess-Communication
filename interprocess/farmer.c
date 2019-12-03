670a56f28a168388a7ce2fc780e97678a1a56ba5
Sonya Zarkova <s.zarkova@student.tue.nl>
2019-12-03 12:37:15 +0200

    Update farmer.c
    farming is left

--- interprocess/farmer.c
+++ interprocess/farmer.c
@@ -2,7 +2,7 @@
  * Operating Systems {2INCO} Practical Assignment
  * Interprocess Communication
  *
- * STUDENT_NAME_1 (STUDENT_NR_1)
+ * Sonya Zarkova 1034611
  * STUDENT_NAME_2 (STUDENT_NR_2)
  *
  * Grading:
@@ -26,6 +26,16 @@
 #include "settings.h"
 #include "common.h"
 
+#define STUDENT_NAME  "SS"
+
+static char                 mq_FW[MQ_MAX_MESSAGES];
+static char                 mq_WF[MQ_MAX_MESSAGES];
+mqd_t               mq_fd_request;
+mqd_t               mq_fd_response;
+int NROF_MESSAGES = 0;
+int NROF_RECEIVED_MESSAGES = 0;
+char JOB_CHAR = ALPHABET_START_CHAR; 
+
 
 int main (int argc, char * argv[])
 {
@@ -36,14 +46,69 @@ int main (int argc, char * argv[])
         
     // TODO:
     //  * create the message queues (see message_queue_test() in interprocess_basic.c)
+
+	    pid_t               processID[NROF_WORKERS];      /* Process ID from fork() */
+	    mqd_t               mq_farmer_worker;
+	    mqd_t               mq_worker_farmer;
+	    MQ_REQUEST_MESSAGE  req;
+	    MQ_RESPONSE_MESSAGE rsp;
+	    struct mq_attr      attr;
+
+	    sprintf (mq_FW, "/mq_request_%s_%d", STUDENT_NAME, getpid());
+	    sprintf (mq_WF, "/mq_response_%s_%d", STUDENT_NAME, getpid());
+
+	    attr.mq_maxmsg  = MAX_MESSAGE_LENGTH;
+	    attr.mq_msgsize = sizeof (MQ_REQUEST_MESSAGE);
+	    mq_fd_request = mq_open (mq_FW, O_WRONLY | O_CREAT | O_EXCL, 0600, &attr);
+
+	    attr.mq_maxmsg  = MAX_MESSAGE_LENGTH;
+	    attr.mq_msgsize = sizeof (MQ_RESPONSE_MESSAGE);
+	    mq_fd_response = mq_open (mq_WF, O_RDONLY | O_CREAT | O_EXCL, 0600, &attr);
+
+	  
     //  * create the child processes (see process_test() and message_queue_test())
+
+	printf ("parent pid:%d\n", getpid()); //noneed
+	for (int i=0; i< NROF_WORKERS; i++){
+		processID[i] = fork();
+
+        if (processID[i] < 0)
+	    {
+		perror("fork() failed");
+		exit (1);
+	    }
+	    else
+	    {
+		if (processID[i] == 0)
+		{
+		    printf ("child  pid:%d\n", getpid()); //noneed
+		    execlp ("./worker", "./worker", "mq_FW" ,"mq_WF", NULL);
+		   		    
+		    // we should never arrive here...
+		    perror ("execlp() failed");
+		}
+	    }
+
+	}   
+
     //  * do the farming
+
+	
     //  * wait until the chilren have been stopped (see process_test())
+
+ 	for( int i=0; i < NROF_WORKERS; i++){
+		waitpid (processID[i], NULL, 0);   // wait for the child 
+		printf ("child %d has been finished\n\n", processID[i]);
+	}
     //  * clean up the message queues (see message_queue_test())
+	    mq_close (mq_fd_response);
+            mq_close (mq_fd_request);
+            mq_unlink (mq_WF);
+            mq_unlink (mq_FW);  
+	
 
     // Important notice: make sure that the names of the message queues contain your
     // student name and the process id (to ensure uniqueness during testing)
     
     return (0);
 }
-
