72c37decb1105820ee34dbb9016df230c8c65682
Sonya Zarkova <s.zarkova@student.tue.nl>
2019-12-03 12:40:19 +0200

    Update common.h
    not done completely

--- interprocess/common.h
+++ interprocess/common.h
@@ -16,6 +16,18 @@
 
 // TODO: put your definitions of the datastructures here
 
+typedef struct
+{
+    char A_START_CHAR;
+    char A_END_CHAR;
+    char WORD_START_CHAR;
+    int WORD_LENGTH;
+    uint128_t MD5;
+} MQ_REQUEST_MESSAGE;       // would need change maybe
+
+typedef struct
+{
+    
+} MQ_RESPONSE_MESSAGE; //would need change maybe
 
 #endif
-
